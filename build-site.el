afc87e2b0f28a0d9aaf8a0b8361beb1789e4763e
Benj <Benjamin.Schwerdtner@gmail.com>
2022-09-23 10:58:16 +0200

    Delete comments

--- build-site.el
+++ build-site.el
@@ -23,11 +23,11 @@
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
-             :with-author nil ;; Don't include author name
-             :with-creator t ;; Include Emacs and Org versions in footer
-             :with-toc t     ;; Include a table of contents
-             :section-numbers nil    ;; Don't include section numbers
-             :time-stamp-file nil)))    ;; Don't include time stamp in file
+             :with-author nil
+             :with-creator t
+             :with-toc t
+             :section-numbers nil
+             :time-stamp-file nil)))
 
 ;; Customize the HTML output
 
