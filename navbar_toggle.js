7fdd11c2b7097532a887230444a2b5fbc6086725
Benj <Benjamin.Schwerdtner@gmail.com>
2023-01-04 08:04:44 +0100

    Add cname

--- navbar_toggle.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector('#navbar-toggle');
-const menu = document.querySelector('#navbar');
-
-toggle.addEventListener('click', function(_e) {
-    console.log("hello");
-  menu.classList.toggle('is-open');
-});
