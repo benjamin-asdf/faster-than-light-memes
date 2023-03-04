9e7344ce40953d92a1a2f2c09c330a3bd8c28cc8
Benj <Benjamin.Schwerdtner@gmail.com>
2023-03-04 17:22:24 +0100

    Update

--- src/ftlmemes/gen_pages.clj
+++ src/ftlmemes/gen_pages.clj
@@ -10,12 +10,9 @@
                                      (map str)
                                      (filter #(re-find #"\.clj$" %))
                                      (mapcat load-file))
-                                    (file-seq
-                                     (fs/file
-                                      "/home/benj/repos/faster-than-light-memes/src/ftlmemes/page")))
+                                    (file-seq (fs/file "src/ftlmemes/page")))
         :let [file (fs/file output-dir file)]]
   (println (str file))
   (spit file content))
-
 
 ;; http://localhost:8081/hire-benjamin.html
