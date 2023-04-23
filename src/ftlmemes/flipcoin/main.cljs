8154b099c41d9f5f9cf472e91d0dd266dd383224
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2023-04-23 20:15:32 +0200

    Inline the svg

--- src/ftlmemes/flipcoin/main.cljs
+++ src/ftlmemes/flipcoin/main.cljs
@@ -28,8 +28,12 @@
                 :height "20vh"}}
          (if
              (:current-coin v)
-             [:img {:alt "coin.svg" :src "flipcoin/coin.svg"}]
-             [:img {:alt "coin-tails.svg" :src "flipcoin/coin-tails.svg"}])
+             [:svg {:xmlns "http://www.w3.org/2000/svg" :width "100" :height "100" :viewBox "0 0 100 100"}
+              [:circle {:cx "50" :cy "50" :r "50" :fill "magenta"}]
+              [:text {:x "50%" :y "50%" :text-anchor "middle" :dy ".3em" :font-size "20" :fill "#000"} "HEADS"]]
+             [:svg {:xmlns "http://www.w3.org/2000/svg" :width "100" :height "100" :viewBox "0 0 100 100"}
+              [:circle {:cx "50" :cy "50" :r "50" :fill "#90AFC5"}]
+              [:text {:x "50%" :y "50%" :text-anchor "middle" :dy ".3em" :font-size "20" :fill "#000"} "TAILS"]])
          [:button
           {:style {:margin-top "1rem"}
            :on-click
